739e9acecce2fdf6bc9df354b72bb7e42663c933
GaneshAchyuth <ganeshachyuth@gmail.com>
2022-02-13 23:49:46 +0530

    Initial Commit

---
+++ first_sql.sql
@@ -0,0 +1,2 @@
+SELECT  Orderdate,DATEADD(DAY, 8 - DATEPART(WEEKDAY, Orderdate), CAST(Orderdate AS DATE)) [End_Dateof_Week] ,
+ Cast( DATEADD(MM,DATEDIFF(MM, -1, Orderdate),-1) as Date) as  Last_dayothe_month from [dbo].[Orders]
