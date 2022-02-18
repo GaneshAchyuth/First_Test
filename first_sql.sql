0fdc3a201b185543aecb9cbcb1eef29f7e64aba9
GaneshAchyuth <ganeshachyuth@gmail.com>
2022-02-18 11:15:54 +0530

    added semi colon to my sql script

--- first_sql.sql
+++ first_sql.sql
@@ -1,2 +1,3 @@
+-- my sql
 SELECT  Orderdate,DATEADD(DAY, 8 - DATEPART(WEEKDAY, Orderdate), CAST(Orderdate AS DATE)) [End_Dateof_Week] ,
  Cast( DATEADD(MM,DATEDIFF(MM, -1, Orderdate),-1) as Date) as  Last_dayothe_month from [dbo].[Orders];
